3f90395fc61ecf39fd1c5d4e9000db5e280833b9
Administrator <1570595476@qq.com>
2015-03-25 15:56:38 +0800

    add

--- app/src/main/java/com/example/administrator/chen/MainActivity.java
+++ app/src/main/java/com/example/administrator/chen/MainActivity.java
@@ -1,5 +1,10 @@
 package com.example.administrator.chen;
 
+
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.Context;
+import android.net.Uri;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
@@ -8,22 +13,37 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
     private Toolbar toolbar;
+    private EditText ed1;
+    private EditText ed2;
+    private EditText ed3;
+    private ContentResolver cr;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ed1 = (EditText) findViewById(R.id.editText1);
+        ed2 = (EditText) findViewById(R.id.editText2);
+        ed3 = (EditText) findViewById(R.id.editTex3);
+        View bt = findViewById(R.id.button);
+
+
+        /////数据库的建立
         toolbar = (Toolbar) findViewById(R.id.tl_custom);
         toolbar.setTitle("首页");
         setSupportActionBar(toolbar);//替代ActionBar
         getSupportActionBar().setHomeButtonEnabled(true); //设置返回键可用
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer);
-        ActionBarDrawerToggle actionBarDrawerToggle= new ActionBarDrawerToggle(this, drawer,toolbar, R.string.app_name, R.string.app_name) {
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.app_name, R.string.app_name) {
             @Override
             public void onDrawerClosed(View drawerView) {
                 super.onDrawerClosed(drawerView);
@@ -38,8 +58,28 @@ public class MainActivity extends ActionBarActivity {
         };
         drawer.setDrawerListener(actionBarDrawerToggle);
         actionBarDrawerToggle.syncState();
-    }
+        cr = getContentResolver();
 
+
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = ed1.getText().toString();
+                String passwprd = ed2.getText().toString();
+                String age = ed3.getText().toString();
+                if (name.isEmpty() ||passwprd.isEmpty() || age.isEmpty()) {
+                    Toast.makeText(MainActivity.this, "数据不能为空", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                Uri uri = Uri.parse("content://com.example.administrator.chen.MyContentProvider/test");
+                ContentValues values = new ContentValues();
+                values.put("name", "陈玖旭");
+                values.put("password", "123456");
+                values.put("age", "23");
+                Uri a = cr.insert(uri, values);
+            }
+        });
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -62,4 +102,6 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
 }
---
+++ app/src/main/java/com/example/administrator/chen/MyContentProvider.java
@@ -0,0 +1,117 @@
+package com.example.administrator.chen;
+
+import android.content.ContentProvider;
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.net.Uri;
+
+/**
+ * Created by Administrator on 2015/3/24.
+ */
+public class MyContentProvider extends ContentProvider {
+    public static String CHEN_TEST = "com.example.administrator.chen.MyContentProvider";
+    private openDB opendb;//数据库帮助类
+    private static UriMatcher um = new UriMatcher(UriMatcher.NO_MATCH);//uri筛选器
+    private static final int tag1 = 1;
+    private static final int tag2 = 2;
+
+    static {
+        um.addURI(CHEN_TEST, "test", tag1);
+        um.addURI(CHEN_TEST, "test/*", tag2);
+    }
+    @Override
+    public boolean onCreate() {
+        opendb = new openDB(getContext());
+        return true;
+    }
+    @Override
+    public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
+        SQLiteDatabase datasql = opendb.getReadableDatabase();
+        SelectionBuilder selectionBuilder = new SelectionBuilder();
+        int TAG = um.match(uri);
+        switch (TAG) {
+            case tag1:
+                String id = uri.getLastPathSegment();
+                selectionBuilder.where("id =?", id);
+            case tag2:
+                selectionBuilder.table("chentest").where(selection, selectionArgs);
+                Cursor cursor = selectionBuilder.query(datasql, projection, sortOrder);
+                Context ctx = getContext();
+                assert ctx != null;
+                cursor.setNotificationUri(ctx.getContentResolver(), uri);
+                return cursor;
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
+    }
+    @Override
+    public String getType(Uri uri) {
+        String type;
+        int tag = um.match(uri);
+        switch (tag) {
+            case tag1:
+                type = ContentResolver.CURSOR_DIR_BASE_TYPE + "/test";
+                return type;
+            case tag2:
+                type = ContentResolver.CURSOR_DIR_BASE_TYPE + "/test.chen";
+                return type;
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
+    }
+    @Override
+    public Uri insert(Uri uri, ContentValues values) {
+        final SQLiteDatabase db = opendb.getWritableDatabase();
+        assert db != null;
+        final int match = um.match(uri);
+        Uri result;
+        switch (match) {
+            case tag1:
+                long id = db.insertOrThrow("chentest", null, values);
+                result = Uri.parse("content://" + CHEN_TEST + "/" + id);//指向这条数据的UR
+                break;
+            case tag2:
+                throw new UnsupportedOperationException("Insert not supported on URI: " + uri);
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
+        Context ctx = getContext();
+        assert ctx != null;
+        ctx.getContentResolver().notifyChange(uri, null, false);
+        return result;
+
+    }
+    @Override
+    public int delete(Uri uri, String selection, String[] selectionArgs) {
+
+
+        return 0;
+        }
+    @Override
+    public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
+        return 0;
+    }
+
+    public class openDB extends SQLiteOpenHelper {
+
+        public openDB(Context context) {
+            super(context, "chenTestSQLiet", null, 1);
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            String sql = "create table chentest( id  INTEGER PRIMARY KEY,name varchar(20) not null,password varchar(60) not null,age varchar(10) not null);";
+            db.execSQL(sql);
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+        }
+    }
+}
